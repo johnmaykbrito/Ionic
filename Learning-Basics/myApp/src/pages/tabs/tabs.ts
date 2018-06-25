3515d58d101a036e2e3bf2d2f8f30bf6fc38715f
John Lima <johnmaykbrito@gmail.com>
2018-06-25 18:54:29 -0300

    Slides, Functions and Buttons
    19th Lesson

--- Learning-Basics/myApp/src/pages/tabs/tabs.ts
+++ Learning-Basics/myApp/src/pages/tabs/tabs.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+// import { AboutPage } from '../about/about';
+// import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import { FeedPage } from '../feed/feed';
+import { IntroPage } from '../intro/intro';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -11,9 +12,10 @@ import { FeedPage } from '../feed/feed';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  // tab2Root = AboutPage;
+  // tab3Root = ContactPage;
   tab4Root = FeedPage;
+  tab5Root = IntroPage;
 
   constructor() {
 
