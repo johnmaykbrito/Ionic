995a0bbae204d1d1c1084a7ee33009748594bed3
John Lima <johnmaykbrito@gmail.com>
2018-06-27 15:17:44 -0300

    Add HttpModule
    $ ionic generate provider Movie

--- Learning-Basics/myApp/src/pages/feed/feed.ts
+++ Learning-Basics/myApp/src/pages/feed/feed.ts
@@ -17,6 +17,15 @@ import { ActionSheetController } from 'ionic-angular';
 })
 export class FeedPage {
 
+  public objeto_feed = {
+    titulo: "John Lima",
+    data: "November 5, 1995",
+    descricao: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.",
+    qntd_likes: 13,
+    qntd_comments: 66,
+    time_comment: "10h ago"
+  }
+
   public buttonValue: string = "BASIC ALERT";
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public actionSheetCtrl: ActionSheetController) {
@@ -24,6 +33,10 @@ export class FeedPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FeedPage');
+  }
+
+  doLike() {
+    this.objeto_feed.qntd_likes++;
   }
 
   doAlert() {
